404f65e8332f765490551b69a005ab5e69afb4c2
Dominik Röschke <dominik.roeschke@cloud37.io>
2022-09-13 16:23:17 +0200

    Tried another cors proxy for getting the zipball

--- src/routes/data/zipball/+server.ts
+++ src/routes/data/zipball/+server.ts
@@ -5,7 +5,7 @@ const apiBaseURL = () => {
 	if (dev) {
 		return 'http://localhost:30080/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
 	} else {
-		return 'https://www.arsnouveau.wiki/api/cors?url=https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
+		return 'https://mighty-river-49444.herokuapp.com/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
 	}
 };
 
