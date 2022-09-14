38e0d70ef867d54cb2c4443612b74e6dd66d7f11
Dominik Röschke <dominik.roeschke@cloud37.io>
2022-09-14 10:12:48 +0200

    Changed URL to only load via internal route during serverside rendering and to load via direct proxy call during client side rendering.

--- src/routes/api/zipball/+server.ts
+++ src/routes/api/zipball/+server.ts
@@ -1,13 +1,5 @@
 import {env} from '$env/dynamic/private';
-import {dev} from '$app/environment';
-
-const apiBaseURL = () => {
-    if (dev) {
-        return 'http://localhost:30080/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
-    } else {
-        return 'https://mighty-river-49444.herokuapp.com/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
-    }
-};
+import {apiBaseURL} from "$lib/utils/apiUtils";
 
 const headers = new Headers({
     Accept: 'application/vnd.github+json',
