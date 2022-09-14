95d0d0101b47db10183494bf0279ae0672be7c56
Dominik Röschke <dominik.roeschke@cloud37.io>
2022-09-14 09:42:05 +0200

    Different CORS-Anywhere proxy provider.

--- src/routes/api/zipball/+server.ts
+++ src/routes/api/zipball/+server.ts
@@ -1,22 +1,22 @@
-import { env } from '$env/dynamic/private';
-import { dev } from '$app/environment';
+import {env} from '$env/dynamic/private';
+import {dev} from '$app/environment';
 
 const apiBaseURL = () => {
-	if (dev) {
-		return 'http://localhost:30080/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
-	} else {
-		return 'https://mighty-river-49444.herokuapp.com/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
-	}
+    if (dev) {
+        return 'http://localhost:30080/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
+    } else {
+        return 'https://cors-anywhere-7sk3.onrender.com/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
+    }
 };
 
 const headers = new Headers({
-	Accept: 'application/vnd.github+json',
-	origin: '*',
-	Authorization: `Bearer: ${env.GITHUB_API_KEY}`
+    Accept: 'application/vnd.github+json',
+    origin: '*',
+    Authorization: `Bearer: ${env.GITHUB_API_KEY}`
 });
-const fetchParameters = { method: 'GET', headers };
+const fetchParameters = {method: 'GET', headers};
 
 /** @type {import('./$types').RequestHandler} */
 export function GET() {
-	return fetch(apiBaseURL(), fetchParameters);
+    return fetch(apiBaseURL(), fetchParameters);
 }
