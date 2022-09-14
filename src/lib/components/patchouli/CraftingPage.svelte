b7b57dc5c40ce8d85b1d984b5a01d0e8df0af0a7
Dominik Röschke <dominik.roeschke@cloud37.io>
2022-09-14 16:05:52 +0200

    There is such a thing as Recipe2 in Patchouli Crafting Pages.

--- src/lib/components/patchouli/CraftingPage.svelte
+++ src/lib/components/patchouli/CraftingPage.svelte
@@ -4,8 +4,14 @@
     import ShapelessRecipe from "$lib/components/patchouli/RecipeDisplay/ShapelessRecipe.svelte";
 
     export let recipe: string;
+    export let recipe2: string;
     $: recipeData = getRecipe(recipe) as App.Recipe;
+    $: recipeData2 = getRecipe(recipe2) as App.Recipe;
     $: displayedComponent = recipeData.type === 'minecraft:crafting_shaped' ? ShapedRecipe : ShapelessRecipe
+    $: displayedComponent2 = recipeData2?.type === 'minecraft:crafting_shaped' ? ShapedRecipe : ShapelessRecipe
 </script>
 
 <svelte:component recipeData={recipeData} this={displayedComponent}/>
+{#if recipe2}
+    <svelte:component recipeData={recipeData2} this={displayedComponent2}/>
+{/if}
