db23af998215c9689a62767bb5251481b3f09f03
Dominik Röschke <dominik.roeschke@cloud37.io>
2022-09-19 08:56:13 +0200

    Revert "Fixed linebreaks on main page in firefox"
    This reverts commit f08e6dfcf5155d5cc757fb6461cd74aca1086382.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,14 @@
 </script>
 
 <div class="flex flex-col justify-center items-start h-full m-2">
-    <p>Welcome to the automatically generated Wiki of Ars Nouveau!<br/><br/>
-        This Wiki is automatically generated from the Patchouli Files in the Ars Nouveau Repository.
+    <p>Welcome to the automatically generated Wiki of Ars Nouveau!</p>
+    <br/>
+    <p>This Wiki is automatically generated from the Patchouli Files in the Ars Nouveau Repository.
         Therefore, there's some things that just can't be displayed, like the GeckoLib-Rendered Spellbook, Blocks,
-        Keybinds and other things.
-        It can also be a bit janky, with missing names or unexpected errors.<br/><br/>
-        If you encounter any issues, please reach out in the Ars Nouveau Discord found <a
-                href="/category/mod_news/entry/mod_news">here!</a></p>
+        Keybinds and
+        other things.
+        It can also be a bit janky, with missing names or unexpected errors.</p>
+    <br/>
+    <p>If you encounter any issues, please reach out in the Ars Nouveau Discord found <a
+            href="/category/mod_news/entry/mod_news">here!</a></p>
 </div>
