7d60b2d9c43ef785e56fff40e5b5b57ee1e2c3ed
Dominik Röschke <dominik.roeschke@cloud37.io>
2022-09-19 09:00:08 +0200

    Fixed Paragraph breaks in Firefox.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
 </script>
 
-<div class="flex flex-col justify-center items-start h-full m-2">
+<div class="flex flex-col justify-center items-start h-full m-2 space-y-2">
     <p>Welcome to the automatically generated Wiki of Ars Nouveau!</p>
-    <br/>
     <p>This Wiki is automatically generated from the Patchouli Files in the Ars Nouveau Repository.
         Therefore, there's some things that just can't be displayed, like the GeckoLib-Rendered Spellbook, Blocks,
         Keybinds and
         other things.
         It can also be a bit janky, with missing names or unexpected errors.</p>
-    <br/>
     <p>If you encounter any issues, please reach out in the Ars Nouveau Discord found <a
             href="/category/mod_news/entry/mod_news">here!</a></p>
 </div>
