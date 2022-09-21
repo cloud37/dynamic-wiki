2fab048b5dbb55d7b2aacc9015d18e4602969afc
Dominik Röschke <dominik.roeschke@cloud37.io>
2022-09-21 13:21:10 +0200

    Fixed broken scrollHelper.

--- src/lib/components/navigation/scrollHelper.ts
+++ src/lib/components/navigation/scrollHelper.ts
@@ -3,7 +3,7 @@ import { browser } from '$app/environment';
 export const scrollSelectionIntoView = () => {
 	if (browser) {
 		setTimeout(() => {
-			const elements = document.getElementsByClassName('!bg-primary-500');
+			const elements = document.getElementsByClassName('navSelected');
 			if (elements && elements.length > 0) {
 				elements[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
 			}
